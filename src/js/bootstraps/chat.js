b9cd07fe08322d01d7979c0628a54d913f405ecb
Chris Wright <chrisw@networkm.co.uk>
2012-11-05 14:46:59 +0000

    Brought submodule up to date, fixed chat bootstrap

--- src/js/bootstraps/chat.js
+++ src/js/bootstraps/chat.js
@@ -1,30 +1,14 @@
 /*jslint plusplus: true, white: true, browser: true */
 /*global CvPlsHelper, chrome */
 
-(function() {
-
-  "use strict";
-
-  var application, opts, onInit;
-
-  opts = {
-    SettingsDataAccessor: CvPlsHelper.chrome.ContentSettingsDataAccessor,
-    SettingsDataStore: CvPlsHelper.chrome.SettingsDataStore,
-    DefaultSettings: CvPlsHelper.chrome.DefaultSettings,
-    DesktopNotificationDispatcher: CvPlsHelper.chrome.DesktopNotificationDispatcher
-  };
-
-  onInit = function() {
-    // Show update page
-    chrome.extension.sendMessage({method: 'checkUpdate'});
-
-    // Add address bar icon
-    if (pluginSettings.getSetting('showIcon')) {
-      chrome.extension.sendMessage({method: 'showIcon'});
-    }
-  };
-
-  application = CvPlsHelper.ChatApplication(document, opts, onInit);
-  application.start();
-
-}());
+(new CvPlsHelper.ChatApplication(document, {
+  SettingsDataAccessor: CvPlsHelper.chrome.ContentSettingsDataAccessor,
+  SettingsDataStore: CvPlsHelper.chrome.SettingsDataStore,
+  DefaultSettings: CvPlsHelper.chrome.DefaultSettings,
+  DesktopNotificationDispatcher: CvPlsHelper.chrome.DesktopNotificationDispatcher
+}, function(pluginSettings) {
+  chrome.extension.sendMessage({method: 'checkUpdate'});
+  if (pluginSettings.getSetting('showIcon')) {
+    chrome.extension.sendMessage({method: 'showIcon'});
+  }
+})).start();
